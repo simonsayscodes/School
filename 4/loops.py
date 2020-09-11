bafe34b95ba1c8a60129922b5a8e042089e76360
simonsayscodes <70754158+simonsayscodes@users.noreply.github.com>
2020-09-11 14:05:19 +0200

    Update loops.py

--- 4/loops.py
+++ 4/loops.py
@@ -1,2 +1,11 @@
-for x in range (1,101,):
-    print(x)
+for x in range (101):
+    if x % 2 == 0:
+        print(x)
+for x in range (5,79):
+    if x % 3 == 0: 
+        print (x)
+thislist = ["Niklas","Victor","Parvin"]
+thislist.reverse()
+print(thislist)
+
+
