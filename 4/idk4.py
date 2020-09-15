59b9676e837ba43fa5cd9c4095eb58853180fc3b
simonsayscodes <70754158+simonsayscodes@users.noreply.github.com>
2020-09-15 13:42:48 +0200

    Uppgifter

--- 4/idk4.py
+++ 4/idk4.py
@@ -1,6 +1,7 @@
 def my_funciton(x):
-    return 5 * x
+    return 5 * x 
 
 print(my_funciton(3))
 print(my_funciton(5))
 print(my_funciton(9))
+
---
+++ 4/whgjawkg.py
@@ -0,0 +1,9 @@
+for x in range (1,101):
+    if x % 3 == 0 and x % 5 ==0: 
+        print ("FizzBuzz")
+    elif x % 3 == 0:
+        print ("F9zz")
+    elif x % 5 == 0:
+        print ("B9zz")
+    else:
+        print(x)
