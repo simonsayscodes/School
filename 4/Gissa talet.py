cfda2ccbb33bc01ce2edd843a9c009cbae58bcfa
simonsayscodes <70754158+simonsayscodes@users.noreply.github.com>
2020-11-03 08:36:01 +0100

    Gissatalet

---
+++ 4/Gissa talet.py
@@ -0,0 +1,14 @@
+import random 
+z = random.randint(1,1000)
+
+for x in range(5): 
+    Write = int(input())
+
+    if Write == z: 
+        print(" Win ") 
+        break
+    else:
+        print(" Try again ")
+#Skriv en lopp för Write. 
+
+
