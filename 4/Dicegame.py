315464c1260af6309e7147d3ddd525f267569c8a
simonsayscodes <70754158+simonsayscodes@users.noreply.github.com>
2020-11-03 08:35:03 +0100

    dicegame

--- 4/Dicegame.py
+++ 4/Dicegame.py
@@ -5,7 +5,7 @@ Emma =  Emma.split(" ")
 gunnerdice =(int(gunner[0]) + int(gunner[1]))/2 + (int(gunner[2]) + int(gunner[3]))/2
 Emmadice = (int(Emma[0]) + int(Emma[1]))/2 + (int (Emma[2]) +int(Emma[3]))/2
 if gunnerdice == Emmadice:
-    print ("tie lol")
+    print ("tie")
 elif gunnerdice > Emmadice:
     print ("Gunner wins")
 elif Emmadice > gunnerdice:
